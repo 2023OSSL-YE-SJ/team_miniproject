57e775ce13bcb7ad9c859e706c7c96198ea9e08b
SoejeongWoo <131163879+SoejeongWoo@users.noreply.github.com>
2023-05-21 16:55:57 +0900

    Update dietrev.c

--- dietrev.c
+++ dietrev.c
@@ -2,32 +2,31 @@
 #include <string.h>
 #include <stdlib.h>
 #define MENU_SIZE 50
+#define ORDER_SIZE 10
 #define BUFFER_SIZE 50
 
-typedef struct {
-char name[BUFFER_SIZE]; // 이름
-int type; // 종류 (1.샐러드 2.라이스 3.면 4.빵 5.음료)
-int price; // 가격
-double calorie; // 칼로리
-}Product;
+typedef struct
+{
+    char name[BUFFER_SIZE]; // 이름
+    int type;               // 종류 (1.샐러드 2.라이스 3.면 4.빵 5.음료)
+    int price;              // 가격
+    double calorie;         // 칼로리
+} Product;
 
-int adminMenu(); //
-int userMenu(); //
+int adminMenu();  //
+int userMenu();   //
 int selectMode(); // 관리자 모드인지 사용자 모드인지 입력받을 함수
-int selectProductMenu();
+int selectCategory();
 int getCalorie();
-int selectProduct(Product *p[], int productMenu);
-int selectProductByCategoryAndCalorie(Product *p[], int productMenu, int calorie);
-int createProduct(Product *p); // 제품을 추가하는 함수 => 메뉴 선택
-void readProduct(Product *p); // 하나의 제품 출력 함수 => 구매 목록 보기
-int updateProduct(Product *p); // 제품을 수정하는 함수 => 구매 목록 수정
+Product * selectProduct(Product *p[], int productMenu, int calorie);
+int createProduct(Product *p);              // 제품을 추가하는 함수 => 메뉴 선택
+void readProduct(Product *p);               // 하나의 제품 출력 함수 => 구매 목록 보기
+int updateProduct(Product *p);              // 제품을 수정하는 함수 => 구매 목록 수정
 int deleteProduct(Product *p[], int count); // 제품을 삭제하는 함수 => 구매 취소 [환불]
-void saveProduct(Product *p[], int count); //데이터를 파일에 저장 하는 함수
-int loadProduct(Product **p); // 저장된 데이터를 불러오는 함수
-int selectDataNo(Product *p[], int count); // 수정, 삭제 할 번호 선택하는 함수
-void listProduct(Product *p[], int count); // 제품 리스트 출력
-
-// void searchProductTypeAndCalories(Product *p[], int count); // 메뉴 종류, 칼로리 검색
+void saveProduct(Product *p[], int count);  //데이터를 파일에 저장 하는 함수
+int loadProduct(Product **p);               // 저장된 데이터를 불러오는 함수
+int selectDataNo(Product *p[], int count);  // 수정, 삭제 할 번호 선택하는 함수
+void listProduct(Product *p[], int count);  // 제품 리스트 출력
 
 char categoryNames[5][100] = {
     "샐러드",
@@ -37,8 +36,11 @@ char categoryNames[5][100] = {
     "음료"
 };
 
-int main(){
+int main()
+{
     Product *sp[MENU_SIZE] = {0};
+    Product *orders[ORDER_SIZE] = {0};
+    int orderCount = 0;
 
     int count = 0, menu;
     int result = 0;
@@ -46,114 +48,142 @@ int main(){
     int no = 0;
     count = loadProduct(sp);
     index = count;
+
     if (index == 0)
         printf("=>파일 없음\n");
 
-    int mode = selectMode(); 
-    while (1) {
-        if (mode == 1) { // 사용자모드
-            menu = userMenu();
-            if (menu == 0) break;
-            if (menu == 1) { // 메뉴 선택 + 칼로리 검색
-                // 메뉴의 종류를 보여주고 선택하게 한다
-                int productMenu = selectProductMenu();
-                int productNumber = selectProduct(sp, productMenu);
-                // 메뉴의 종류를 고르면 해당 종류의 모든 메뉴를 보여준다
-                // 칼로리를 고를지 선택하게 한다
-                // 메뉴 종류와 카테고리를 기준으로 해당하는 메뉴만 보여준다
-                if (productNumber == 0) { // 칼로리 선택
-                    // 칼로리 입력받기
-                    int calorie = getCalorie();
-                    selectProductByCategoryAndCalorie(sp, productMenu, calorie);
+    int mode = selectMode();
+    int done = 0;
+    while (!done)
+    {
+        if (mode == 1)
+        { // 사용자모드
+            
+            while (1) {
+                menu = userMenu();
+                if (menu == 0)
+                    break;
+                if (menu == 1)
+                { // 메뉴 선택 + 칼로리 검색
+                    // 메뉴의 종류를 보여주고 선택하게 한다
+                    int category = selectCategory();
+
+                    int input;
+                    printf("1. 메뉴 선택\n");
+                    printf("2. 칼로리 선택\n\n");
+                    printf("원하는 메뉴 선택>> ");
+                    scanf("%d", &input);
+
+                    int calorie = 999999;
+                    if (input == 2) {
+                        calorie = getCalorie();
+                    }
+                    Product* product = selectProduct(sp, category, calorie);
+                    orders[orderCount++] = product;
                 }
-                printf("주문이 완료되었습니다.\n");
-                break;
-            } else if (menu == 6) {
-                // searchProductType(sp, index);
-                // searchProductTypeAndCalories(sp, index);
+                else if (menu == 2) // 주문완료
+                {
+                    //
+                    printf("주문이 완료되었습니다.\n\n");
+                    printf("=== 주문내역 ===\n");
+                    int price = 0;
+                    for (int i=0; i<orderCount; i++) {
+                        readProduct(orders[i]);
+                        price += orders[i]->price;
+                    }
+                    printf("총 상품 개수: %d\n", orderCount);
+                    printf("전체 가격: %d\n", price);
+                    printf("================\n\n");
+                    done = 1;
+                    break;
+                }
             }
-        } else if (mode == 2) { // 관리자모드
+
+        }
+        else if (mode == 2)
+        { // 관리자모드
             menu = adminMenu();
-            if (menu == 0) {
+            if (menu == 0)
+            {
                 break;
-            } else if (menu == 1) { // 메뉴 추가
+            }
+            else if (menu == 1)
+            { // 메뉴 추가
                 sp[index] = (Product *)malloc(sizeof(Product));
                 result = createProduct(sp[index]);
                 index++;
                 printf("=> 추가됨!\n");
-            } else if (menu == 2) { // 메뉴 수정
+            }
+            else if (menu == 2)
+            { // 메뉴 수정
                 no = selectDataNo(sp, index);
-                if (no != 0) {
+                if (no != 0)
+                {
                     result = updateProduct(sp[no - 1]);
                     printf("=> 수정됨!\n");
-                } else
+                }
+                else
                     printf("수정을 취소함\n");
-            } else if (menu == 3) { // 메뉴 삭제
+            }
+            else if (menu == 3)
+            { // 메뉴 삭제
                 int n = 0;
                 no = selectDataNo(sp, index);
                 printf("정말로 삭제하시겠습니까? (삭제:1)");
                 scanf("%d", &n);
-                if (n == 1) {
+                if (n == 1)
+                {
                     result = deleteProduct(sp, no);
-                } else
+                }
+                else
                     printf("=> 취소됨!\n");
-            } else if (menu == 4) { // 파일에 저장
+            }
+            else if (menu == 4)
+            { // 파일에 저장
                 saveProduct(sp, index);
             }
-        } else {
+        }
+        else
+        {
             break;
-        }   
+        }
     }
     printf("종료됨!\n");
     return 0;
-    }
+}
 
-    int getCalorie() {
-        int calorie;
-        printf("칼로리를 입력해주세요: ");
-        scanf("%d", &calorie);
-        return calorie;
-    }
+int getCalorie()
+{
+    int calorie;
+    printf("칼로리를 입력해주세요: ");
+    scanf("%d", &calorie);
+    return calorie;
+}
 
-    int selectProduct(Product *p[], int productMenu) {
-        int index = 0;
-        int menu;
-        printf("0. 칼로리 선택\n");
-        int number = 1;
+Product* selectProduct(Product *p[], int category, int calorie)
+{
+    int index = 0;
+    int menu;
+    int number = 1;
 
-        while (p[index] && index < MENU_SIZE) {
-            if (p[index]->type == productMenu) {
-                printf("%d. ", number++);
-                readProduct(p[index]);
-            }
-            index++;
+    while (p[index] && index < MENU_SIZE)
+    {
+        if (p[index]->type == category && p[index]->calorie <= calorie)
+        {
+            printf("%d. ", index+1);
+            readProduct(p[index]);
         }
-
-        printf("\n=> 원하는 메뉴는? ");
-        scanf("%d", &menu);
-        return menu;
-    }
-
-    int selectProductByCategoryAndCalorie(Product *p[], int productMenu, int calorie) {
-        int index = 0;
-        int menu;
-        int number = 1;
-
-        while (p[index] && index < MENU_SIZE) {
-            if (p[index]->type == productMenu && p[index]->calorie <= calorie) {
-                printf("%d. ", number++);
-                readProduct(p[index]);
-            }
-            index++;
+        index++;
     }
 
     printf("\n=> 원하는 메뉴는? ");
     scanf("%d", &menu);
-    return menu;
+    return p[menu - 1];
 }
 
-int selectProductMenu() {
-    int productMenu;
+int selectCategory()
+{
+    int category;
     printf("\n*** 다이어트 식당 메뉴 ***\n");
     printf("1. 샐러드\n");
     printf("2. 라이스\n");
@@ -161,11 +191,12 @@ int selectProductMenu() {
     printf("4. 빵\n");
     printf("5. 음료\n\n");
     printf("=> 원하는 메뉴는? ");
-    scanf("%d", &productMenu);
-    return productMenu;
+    scanf("%d", &category);
+    return category;
 }
 
-int selectMode(){
+int selectMode()
+{
     int mode;
     printf("\n*** 모드 선택 ***\n");
     printf("1. 사용자 모드\n");
@@ -176,17 +207,20 @@ int selectMode(){
     return mode;
 }
 
-int userMenu(){
+int userMenu()
+{
     int menu;
     printf("\n*** 사용자 메뉴 ***\n");
     printf("1. 메뉴 종류 + 칼로리 검색\n");
+    printf("2. 주문 완료\n");
     printf("0. 종료\n\n");
     printf("=> 원하는 메뉴는? ");
     scanf("%d", &menu);
     return menu;
 }
 
-int adminMenu(){
+int adminMenu()
+{
     int menu;
     printf("\n*** 메뉴 관리 ***\n");
     printf("1. 추가\n");
@@ -199,7 +233,8 @@ int adminMenu(){
     return menu;
 }
 
-int selectDataNo(Product *p[], int count){
+int selectDataNo(Product *p[], int count)
+{
     int no;
     listProduct(p, count);
     printf("번호는 (취소:0)? ");
@@ -207,7 +242,8 @@ int selectDataNo(Product *p[], int count){
     return no;
 }
 
-int createProduct(Product *p){
+int createProduct(Product *p)
+{
     printf("메뉴 종류는? (1.샐러드 2.라이스 3.면 4.빵 5.음료) ");
     scanf("%d", &p->type);
     getchar(); // 엔터 제거
@@ -220,63 +256,76 @@ int createProduct(Product *p){
     scanf("%lf", &p->calorie);
     return 1;
 }
-void listProduct(Product *p[], int count){
+void listProduct(Product *p[], int count)
+{
     printf("********************************************\n");
-    for(int i=0; i<count; i++){
-    if(p[i] == NULL) continue;
-    printf("%2d ", i+1);
-    readProduct(p[i]);
+    for (int i = 0; i < count; i++)
+    {
+        if (p[i] == NULL)
+            continue;
+        printf("%2d ", i + 1);
+        readProduct(p[i]);
     }
-    printf("\n");
-    }
-
-void readProduct(Product *p){
-    printf("%20s | ", p->name);
-    printf("%10s | ", categoryNames[p->type - 1]);
-    printf("%5d | ", p->price);
-    printf("%7.1lf", p->calorie);
     printf("\n");
 }
 
-int updateProduct(Product *p){
+void readProduct(Product *p)
+{
+    printf("%-20s", p->name);
+    printf("%-10s", categoryNames[p->type - 1]);
+    printf("%-10d", p->price);
+    printf("%-10.1lf", p->calorie);
+    printf("\n");
+}
+
+int updateProduct(Product *p)
+{
     int result = createProduct(p);
     return 1;
 }
 
-int deleteProduct(Product *p[], int count){
-    if(p[count-1]) free(p[count-1]);
-    p[count-1] = NULL;
+int deleteProduct(Product *p[], int count)
+{
+    if (p[count - 1])
+        free(p[count - 1]);
+    p[count - 1] = NULL;
     printf("=> 삭제됨!\n");
     return 0;
 }
 
-void saveProduct(Product *p[], int count){
+void saveProduct(Product *p[], int count)
+{
     FILE *fp;
     fp = fopen("menu.txt", "wt");
 
-    for(int i=0; i<count; i++){
-        if(p[i] == NULL) continue;
+    for (int i = 0; i < count; i++)
+    {
+        if (p[i] == NULL)
+            continue;
         fprintf(fp, "%s\n%d %d %.1lf\n",
-        p[i]->name, p[i]->type, p[i]->price, p[i]->calorie);
+                p[i]->name, p[i]->type, p[i]->price, p[i]->calorie);
     }
     fclose(fp);
     printf("=> 저장됨! ");
-    }
+}
 
-int loadProduct(Product **p){
+int loadProduct(Product **p)
+{
     FILE *fp;
     fp = fopen("menu.txt", "rt");
-    if(fp == NULL) return 0; // 파일이 없는 경우
+    if (fp == NULL)
+        return 0; // 파일이 없는 경우
     int i = 0;
-    while(!feof(fp)) {
-    p[i] = (Product *) malloc(sizeof(Product)); // 메모리 할당
-    fgets(p[i]->name, BUFFER_SIZE, fp);
-    p[i]->name[strlen(p[i]->name) - 1] = '\0';
-    fscanf(fp, "%d", &p[i]->type);
-    fscanf(fp, "%d", &p[i]->price);
-    fscanf(fp, "%lf\n", &p[i]->calorie);
-    // printf("%s %d %d %lf", p[i]->name, p[i]->type, p[i]->price, p[i]->calorie);
-    i++;
+    while (!feof(fp))
+    {
+        p[i] = (Product *)malloc(sizeof(Product)); // 메모리 할당
+        fgets(p[i]->name, BUFFER_SIZE, fp);
+        p[i]->name[strlen(p[i]->name) - 1] = '\0';
+        fscanf(fp, "%d", &p[i]->type);
+        fscanf(fp, "%d", &p[i]->price);
+        fscanf(fp, "%lf\n", &p[i]->calorie);
+        // printf("%s %d %d %lf", p[i]->name, p[i]->type, p[i]->price, p[i]->calorie);
+        i++;
     }
     fclose(fp);
     printf("=> 로딩 성공!\n");
